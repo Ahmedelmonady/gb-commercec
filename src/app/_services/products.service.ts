1664ea0e2b6a56d71fd025627ceba51f314b83cf
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2023-01-20 01:21:16 +0200

    Implemented GetCategoryProducts http call

--- src/app/_services/products.service.ts
+++ src/app/_services/products.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GetCategoryProductsDto } from '../_models/get-category-products-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  baseUrl = "https://localhost:7203/api/Products/"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCategoryProducts(model: GetCategoryProductsDto){
+    return this.httpClient.post(this.baseUrl+"CategoryProducts", model);
+  }
 }
