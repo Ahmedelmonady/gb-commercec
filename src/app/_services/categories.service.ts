e2faaa926d59c337b9bf82f53ba6065aa1708e15
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2023-01-19 13:07:20 +0200

    Added Categories service implementing add new category and get all categories network requests

--- src/app/_services/categories.service.ts
+++ src/app/_services/categories.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CategoryResponse } from '../_models/category-response';
+import { NewCategoryDto } from '../_models/new-category-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  baseUrl = "https://localhost:7203/api/Categories/"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllCategories(){
+    return this.httpClient.get<CategoryResponse>(this.baseUrl + "All");
+  }
+
+  addNewCategory(newCategoryDto: NewCategoryDto){
+    return this.httpClient.post(this.baseUrl + "New", newCategoryDto);
+  }
+
+
 }
