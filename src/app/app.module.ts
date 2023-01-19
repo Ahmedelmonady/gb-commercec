a4633dcb24caa35ad6b24a777273adf3e2b6851c
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2023-01-20 01:22:56 +0200

    Added HttpClientModule, ToastrModule, and Views components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FoodComponent } from './food/food.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    FoodComponent,
+    ElectronicsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    ToastrModule.forRoot({
+      positionClass: "toast-bottom-right"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
