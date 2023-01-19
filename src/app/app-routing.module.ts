6605df2e22e6a156d69a076ddbe570ecca4eafe8
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2023-01-20 01:23:13 +0200

    Added Application routing rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { FoodComponent } from './food/food.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "food", component: FoodComponent
+  },
+  {
+    path: "electronics", component: ElectronicsComponent
+  },
+  {
+    path: "", component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
