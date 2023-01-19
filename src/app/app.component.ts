5d9141210337ed8db9ca1bd3fa56b98975a1b4c9
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2023-01-20 01:25:15 +0200

    Defined main html views and implemented get all/add new categories http calls (for testing the endpoints)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryResponse } from './_models/category-response';
+import { NewCategoryDto } from './_models/new-category-dto';
+import { CategoriesService } from './_services/categories.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'e-commerce-client';
+  categories : any;
+  constructor(private categoriesService: CategoriesService){}
+
+  ngOnInit(): void {
+    var newCategory: NewCategoryDto = {
+      name: "Vehiclezs",
+      codename: "VCHX"
+    }
+  }
+
+  addNewCategory(newCategory: NewCategoryDto){
+    this.categoriesService.addNewCategory(newCategory).subscribe({
+      next: resp => {
+        console.log(resp);
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+
+  getAllCategories(){
+    this.categoriesService.getAllCategories().subscribe(
+      {
+        next: response => {
+          this.categories = response;
+          console.log(response);
+        },
+        error: err => {
+          console.log(err);
+        }
+      }
+    );
+  }
 }
