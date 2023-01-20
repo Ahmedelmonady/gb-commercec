25a391cb5eefe86d276a17fc13694e54529a7cdf
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2023-01-20 20:23:09 +0200

    Added Bootstrap dropdown module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { HomeComponent } from './home/home.component';
     HttpClientModule,
     ToastrModule.forRoot({
       positionClass: "toast-bottom-right"
-    })
+    }),
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
